5dbbed3ab1f08be4c8b2b8905c73fdda5f2e1544
Äniebietabasi <aniebietabasiobo@gmail.com>
2025-05-05 09:57:35 +0100

    commit

--- src/components/Modals/StaffDetailModal.tsx
+++ src/components/Modals/StaffDetailModal.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import {  Dialog, DialogPanel,  } from "@headlessui/react";
 
 function StaffDetailModal({
   isOpen,
@@ -21,22 +21,10 @@ function StaffDetailModal({
         <div className="flex min-h-full items-center justify-center p-4">
           <DialogPanel
             transition
-            className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+            className="w-full max-w-md rounded-xl text-white bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
           >
-            <DialogTitle as="h3" className="text-base/7 font-medium text-white">
-              Payment successful
-            </DialogTitle>
-            <p className="mt-2 text-sm/6 text-white/50">
-              Your payment has been successfully submitted. We’ve sent you an
-              email with all of the details of your order.
-            </p>
-            <div className="mt-4">
-              <Button
-                className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
-                onClick={close}
-              >
-                Got it, thanks!
-              </Button>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptas eveniet adipisci laudantium incidunt, nostrum recusandae cum porro esse, molestiae quo asperiores dignissimos, ducimus eum. Excepturi ipsum et dicta delectus?
             </div>
           </DialogPanel>
         </div>
--- src/pages/private/overview.tsx
+++ src/pages/private/overview.tsx
@@ -1,16 +1,64 @@
 // import React from 'react'
 
-import { useState } from "react"
+import { useState } from "react";
 
 function Overview() {
   const [tab, setTab] = useState(0);
   return (
     <div>
-      <div className="p-[10px] flex items-center space-x-[10px]">
-        <button onClick={()=> setTab(0)} className={`${tab === 0 ? "bg-[#EAAF4E] text-black" : "text-white"} p-[8px_20px]`}></button>
+      <div className="flex items-center justify-between">
+        <div className="p-[10px] flex items-center space-x-[10px]">
+          <button
+            onClick={() => setTab(0)}
+            className={`${
+              tab === 0 ? "bg-[#EAAF4E] text-black" : "text-white"
+            } rounded-[8px] p-[8px_20px]`}
+          >
+            Overview
+          </button>
+          <button
+            onClick={() => setTab(1)}
+            className={`${
+              tab === 1 ? "bg-[#EAAF4E] text-black" : "text-white"
+            } rounded-[8px] p-[8px_20px]`}
+          >
+            Patients
+          </button>
+          <button
+            onClick={() => setTab(2)}
+            className={`${
+              tab === 2 ? "bg-[#EAAF4E] text-black" : "text-white"
+            } rounded-[8px] p-[8px_20px]`}
+          >
+            Hospitals
+          </button>
+          <button
+            onClick={() => setTab(3)}
+            className={`${
+              tab === 3 ? "bg-[#EAAF4E] text-black" : "text-white"
+            } rounded-[8px] p-[8px_20px]`}
+          >
+            Doctprs
+          </button>
+        </div>
+
+        <div className="flex items-center space-x-[10px]">
+          <button className="bg-[#EAAF4E] text-black p-[8px_20px] rounded-[8px]">
+            Add New Patient
+          </button>
+          <button className="bg-[#EAAF4E] text-black p-[8px_20px] rounded-[8px]">
+            Validate Patient
+          </button>
+        </div>
       </div>
+
+     <div className="mt-[20px] text-white border p-[20px]">
+     {tab === 0 && (
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati laborum consectetur provident saepe vel perferendis fugit molestias voluptates blanditiis voluptatum nostrum laudantium hic dolor voluptatem, assumenda nulla amet maiores alias!</div>
+      )}
+     </div>
     </div>
-  )
+  );
 }
 
-export default Overview
+export default Overview;
